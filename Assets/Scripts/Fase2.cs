a88a8ae2bed41d9b06c87ca23f104cb3522fe6d4
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2020-11-05 16:40:34 -0300

    Projeto

---
+++ Assets/Scripts/Fase2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fase2 : MonoBehaviour
+{
+
+    public GameObject porta;
+    public GameObject player;
+    public AudioSource winSound;
+    Boolean tokoh = false;
+
+    void Update()
+    {
+        if (player.transform.position.x > porta.transform.position.x && tokoh == false)
+        {
+            PlayEndSound();
+            tokoh = true;
+            StartCoroutine("EndLevel");
+        }
+    }
+
+    void PlayEndSound()
+    {
+        winSound.Play();
+    }
+    IEnumerator EndLevel()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
